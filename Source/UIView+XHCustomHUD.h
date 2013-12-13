36cf09666aab569c07f5a44f8f41ff3cfbdf9159
xhzengAIB <xhzengAIB@gmail.com>
2013-12-13 03:16:11 +0000

    添加注释

--- Source/UIView+XHCustomHUD.h
+++ Source/UIView+XHCustomHUD.h
@@ -9,8 +9,19 @@
 #import <UIKit/UIKit.h>
 #import "XHShockHUDHeader.h"
 
+/**
+ *  已经定制了四种样式的指示器
+ */
 @interface UIView (XHCustomHUD)
 
+/**
+ *  通过文本展示指示器
+ *
+ *  @param text          需要展示的文本
+ *  @param hudType       指示器的样式
+ *  @param animationType 指示器的显示和消失的动画形式
+ *  @param delay         延迟多少秒消失，如果是0，那就是不消失，需要手动消失
+ */
 - (void)showHUDWithText:(NSString *)text
                 hudType:(XHHUDType)hudType
           animationType:(XHHUDAnimationType)animationType
